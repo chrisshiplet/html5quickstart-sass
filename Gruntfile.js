d32e9b6b5e314400996450e344a9047c13eb6b7b
Chris Shiplet <chris@nearengine.com>
2014-09-23 19:29:31 -0700

    Update packages

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            css: {
+                files: 'src/**/*.scss',
+                tasks: ['sass']
+            },
+            scripts : {
+                files: 'src/**/*.js',
+                tasks: ['jshint', 'uglify']
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'public/assets/style.css' : 'src/scss/style.scss'
+                }
+            }
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js'],
+            options: {
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                options: {
+                    sourceMap: true
+                },
+                src : 'src/**/*.js',
+                dest : 'public/assets/scripts.js'
+            }
+        },
+        browserSync: {
+            bsFiles: {
+                src : ['public/**/*.css', 'public/**/*.js', 'public/**/*.html']
+            },
+            options: {
+                server: {
+                    baseDir: "./public"
+                },
+                // use proxy instead of server for apps
+                // proxy: "local.dev",
+                watchTask: true
+            }
+        }
+    });
+    // grunt.loadNpmTasks('grunt-jekyll');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ["sass", "browserSync", "watch"]);
+};
